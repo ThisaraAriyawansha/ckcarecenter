98a8f6fcde148fc9ba43d9346bece74802a76902
Thisara Ariyawansha <thisara.a2001@gmail.com>
2026-05-27 14:58:54 +0530

    Update AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,7 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->brandLogo(asset('logo/logo_2.png'))
             ->brandLogoHeight('3rem')
             ->colors([
-                'primary' => Color::Sky,
+                'primary' => Color::hex('#1C3F6E'),
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
