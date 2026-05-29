9c12408d73970240f086dbac642ccd60c9ea2101
Thisara Ariyawansha <thisara.a2001@gmail.com>
2026-05-29 08:58:22 +0530

    Update header.blade.php

--- resources/views/frontend/components/header.blade.php
+++ resources/views/frontend/components/header.blade.php
@@ -24,7 +24,7 @@
                             </li>
 
                             <li class="nav-item">
-                                <a class="nav-link {{ request()->routeIs('contact') ? 'active' : '' }}" href="{{ route('contact') }}">Contact Us</a>
+                                <a class="nav-link {{ request()->routeIs('contact') ? 'active' : '' }}" href="{{ route('contact') }}">Contact us</a>
                             </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle {{ request()->routeIs('faq', 'blog', 'gallery', 'testimonial') ? 'active' : '' }}" href="#" role="button" data-bs-toggle="dropdown"
